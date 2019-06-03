4685988bceb7673a1be8cfbf7926344b86e612c2
Patrick Rose <patrick@edlabs.de>
2019-06-03 11:08:27 +0200

    Load config from file

--- resonanzkontrolle/__main__.py
+++ resonanzkontrolle/__main__.py
@@ -1,8 +1,32 @@
+import os
+
 import bjoern
 
 import resonanzkontrolle.resonanzkontrolle as resonanzkontrolle
 
 
+# Load config
+for filepath in (
+    os.environ.get("RESONANZKONTROLLE_CONF"),
+    os.path.normpath(
+        os.path.dirname(os.path.realpath(__file__)) + '/../etc'
+    ) + '/resonanzkontrolle.conf'
+):
+    if filepath is None:
+        continue
+    configfile = os.path.expanduser(filepath)
+    if os.path.isfile(configfile):
+        config = resonanzkontrolle.load_config(configfile)
+        break
+else:
+    config = resonanzkontrolle.load_config()
+
+
 # Run WSGI server with app
-app = resonanzkontrolle.create_app()
-bjoern.run(app, 'localhost', 8002)
+app = resonanzkontrolle.create_app(config)
+print(
+    'Starting Resonanzkontrolle, ',
+    'listening on ', config['host'], ':', config['port'], '.',
+    sep=''
+)
+bjoern.run(app, config['host'], config['port'])
--- resonanzkontrolle/resonanzkontrolle.py
+++ resonanzkontrolle/resonanzkontrolle.py
@@ -1,6 +1,35 @@
+import configparser
+import secrets
+
 import flask
 
 
-def create_app():
+def load_config(configfile=None):
+    configfile_parser = configparser.ConfigParser()
+    if configfile is not None:
+        configfile_parser.read(configfile)
+    config = {}
+    config['hostname'] = configfile_parser.get(
+        'Resonanzkontrolle', 'hostname', fallback=''
+    )
+    config['data_dir'] = configfile_parser.get(
+        'Resonanzkontrolle', 'data_dir', fallback='data'
+    )
+    config['secret_key'] = configfile_parser.get(
+        'Resonanzkontrolle', 'secret_key', fallback=secrets.token_bytes(32)
+    )
+    config['host'] = configfile_parser.get(
+        'Resonanzkontrolle', 'host', fallback='localhost'
+    )
+    config['port'] = configfile_parser.getint(
+        'Resonanzkontrolle', 'port', fallback=8002
+    )
+    return config
+
+
+def create_app(config):
     app = flask.Flask('resonanzkontrolle')
+    app.config['HOSTNAME'] = config['hostname']
+    app.config['DATA_DIR'] = config['data_dir']
+    app.config['SECRET_KEY'] = config['secret_key']
     return app
